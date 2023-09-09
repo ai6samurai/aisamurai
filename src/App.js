3dd625d6a04218266293f12bfa1fca06542d9e81
aditisadwelkar <aditi.sadwelkar@gmail.com>
2023-09-09 09:54:03 -0700

    testing changes to home page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          This website is created for AI Samurai's GenLab Hackathon project.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
